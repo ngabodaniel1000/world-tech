529ab85f010a93589881c2dec32e4e259768ee39
ngabodaniel1000 <ngabodaniel1000@gmail.com>
2025-02-03 15:33:59 +0200

    new chance

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
+// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: './', // Ensure this is correct
-  build: {
-    outDir: 'dist'
-  },
-  server: { 
-    historyApiFallback: true, // Ensure React Router works on Render
-
-   }
 })
+
